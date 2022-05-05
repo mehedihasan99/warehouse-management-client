de4cca06de4ee96d7c285d926e0f1ee496bc1d7f
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-05-06 00:53:30 +0600

    add banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Carousel } from 'react-bootstrap';
-import banner1 from '../../../images/banner/banner-1.jpg'
-import banner2 from '../../../images/banner/banner-2.jpg'
+import banner1 from '../../../images/banner/banner-1.png'
+import banner2 from '../../../images/banner/banner-2.png'
 import banner3 from '../../../images/banner/banner-3.png'
 
 const Banner = () => {
@@ -14,40 +14,37 @@ const Banner = () => {
         <Carousel activeIndex={index} onSelect={handleSelect}>
             <Carousel.Item>
                 <img
-                    style={{ height: '530px' }}
                     className="d-block w-100"
                     alt="First slide"
                     src={banner1}
                 />
                 <Carousel.Caption>
-                    <h3>Hooded Sweatshirt</h3>
-                    <p>Made of cotton & polyester fabrics.Dry function is added which helps to dry faster evene after waashing or sweating..</p>
+                    <h3 className='text-black'>Hooded Sweatshirt</h3>
+                    <p className='text-black'>Made of cotton & polyester fabrics.Dry function is added which helps to dry faster evene after waashing or sweating..</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                    style={{ height: '530px' }}
                     className="d-block w-100"
                     alt="Second slide"
                     src={banner2}
                 />
 
                 <Carousel.Caption>
-                    <h3>Cooldry Crop</h3>
-                    <p>Water Repellent Fabric, Dry Repellent Fabric & Wrinkle Resistant</p>
+                    <h3 className='text-black'>Cooldry Crop</h3>
+                    <p className='text-black'>Water Repellent Fabric, Dry Repellent Fabric & Wrinkle Resistant</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                    style={{ height: '530px' }}
                     className="d-block w-100"
                     src={banner3}
                     alt="Third slide"
                 />
 
                 <Carousel.Caption>
-                    <h3>Singlet</h3>
-                    <p>
+                    <h3 className='text-black'>Singlet</h3>
+                    <p className='text-black'>
                         Made from 100% cotton fabrics with printed pattern in various colors.
                     </p>
                 </Carousel.Caption>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import './Header.css'
 const Header = () => {
     return (
-        <div>
-            <h2>This is header</h2>
-        </div>
+        <>
+            <Navbar className='py-4' sticky='top' collapseOnSelect expand="lg" bg="secondary" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Perfect Size</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">More deets</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Dank memes
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
