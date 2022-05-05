868078a0d9d69eced9e08cc604d2f5282475e31c
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-05-06 03:24:45 +0600

    add register form

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -5,13 +5,20 @@ const Register = () => {
     const nameRef = useRef('');
     const emailRef = useRef('');
     const passRef = useRef('');
+    const handleRegister = event => {
+        event.preventDefault();
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const pass = passRef.current.value;
+        console.log(name, email, pass);
+    }
     return (
         <div className='container main-container'>
-            <form className='form-container'>
+            <form onSubmit={handleRegister} className='form-container'>
                 <h3 className='text-center mb-5'>Register</h3>
-                <input type="text" name="name" placeholder='Your Name' />
-                <input type="email" name="email" placeholder='Your Email' required />
-                <input type="password" name="password" placeholder='Your Password' />
+                <input type="text" ref={nameRef} name="name" placeholder='Your Name' />
+                <input type="email" ref={emailRef} name="email" placeholder='Your Email' required />
+                <input type="password" ref={passRef} name="password" placeholder='Your Password' />
                 <input type="submit" value="Register" required />
                 <p className='text-center mt-4'>
                     <span className='text-primary'>Already have a account?</span>
