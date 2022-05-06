6bc099714e1c80c5e11158e03b35f178b1880d9d
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-05-06 12:42:06 +0600

    add google sign in

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate()
+    let errorMsg;
+    if (error) {
+
+        errorMsg = <p>Error: {error.message}</p>
+
+    }
+
+    if (user) {
+        navigate('/home')
+    }
     return (
         <div>
             <div className='d-flex align-items-center'>
@@ -8,8 +23,9 @@ const SocialLogin = () => {
                 <span className='mx-2'>OR</span>
                 <div style={{ height: '1px' }} className='bg-primary w-50'></div>
             </div>
+            <p className='text-danger text-center'>{errorMsg}</p>
             <div>
-                <button className='btn btn-primary w-100 mt-4'>Google Sign In</button>
+                <button onClick={() => signInWithGoogle()} className='btn btn-primary w-100 mt-4'>Google Sign In</button>
             </div>
         </div>
     );
